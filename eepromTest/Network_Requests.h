0e0708245c8cb5f607fff899341fb8cd73d5efa9
Michael Iodice <miodice3@gmail.com>
2021-04-29 11:31:49 -0600

    visit mozilla developer docs, review post request format, implement correct format for post assemblage within string, confirm params are now being recieved correctly in rails server and no longer lumped as key due to missing ampersand

--- eepromTest/Network_Requests.h
+++ eepromTest/Network_Requests.h
@@ -3,10 +3,7 @@
 
 #include <WiFi.h>
 
-//#include <HttpClient.h>
 #include <HTTPClient.h>
-
-
 
 #include "MAC_Address.h"
 
@@ -38,10 +35,38 @@ class Network_Request{
   void LATCH(){
     Serial.print("chips id is:: "); Serial.println(DEVICE.ID());
     HTTPClient  client;
-    client.begin("http://192.168.0.33:8000/appliances");
-    client.GET();
-    }  
+//  ifconfig |grep inet  
+    String url = "http://192.168.0.29:8000/appliances/";
+
+//    String payload = "{1,value1},{2,value2},{3,value3}";
+    String payload = "appliance=thisvalue&3=thisothervalue";
+    String httpRequestData = "value1=24.25&value2=49.54&value3=1005.14";
+    
+    client.begin(url);
+    
+//    many tutorials are assembling dynamic URLS, passing in as so:
+//    http.begin(serverPath.c_str());
+
+//    client.GET();
+//    client.addHeader("Content-Type", "application/json");
+    client.POST(payload);
+//      client.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");
+    }
 
 };
 
 Network_Request   NETWORK;
+
+//
+//HTTP POST JSON Object
+//Or if you prefer to make an HTTP POST request with a JSON object:
+//
+//POST /update-sensor HTTP/1.1
+//Host: example.com
+//{api_key: "tPmAT5Ab3j7F9", sensor_name: "BME280", temperature: 24.25; humidity: 49.54; pressure: 1005.14}
+//Content-Type: application/json
+//Use the next snippet:
+//
+//http.addHeader("Content-Type", "application/json");
+//
+//int httpResponseCode = http.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");
